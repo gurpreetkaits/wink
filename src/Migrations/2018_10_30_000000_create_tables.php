1a326d09ab58a15834b55b89ddf0b792f976eac8
Gurpreet Kait <gurpreetkait.codes@gmail.com>
2025-01-06 00:05:35 +0530

    fix: uuid issue in migration

--- src/Migrations/2018_10_30_000000_create_tables.php
+++ src/Migrations/2018_10_30_000000_create_tables.php
@@ -14,7 +14,7 @@ class CreateTables extends Migration
     public function up()
     {
         Schema::create('wink_tags', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->string('id')->primary();
             $table->string('slug')->unique();
             $table->string('name');
             $table->timestamps();
@@ -23,14 +23,14 @@ class CreateTables extends Migration
         });
 
         Schema::create('wink_posts_tags', function (Blueprint $table) {
-            $table->uuid('post_id');
-            $table->uuid('tag_id');
+            $table->string('post_id');
+            $table->string('tag_id');
 
             $table->unique(['post_id', 'tag_id']);
         });
 
         Schema::create('wink_posts', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->string('id')->primary();
             $table->string('slug')->unique();
             $table->string('title');
             $table->text('excerpt');
@@ -39,12 +39,12 @@ class CreateTables extends Migration
             $table->dateTime('publish_date')->default('2018-10-10 00:00:00');
             $table->string('featured_image')->nullable();
             $table->string('featured_image_caption');
-            $table->uuid('author_id')->index();
+            $table->string('author_id')->index();
             $table->timestamps();
         });
 
         Schema::create('wink_authors', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->string('id')->primary();
             $table->string('slug')->unique();
             $table->string('name');
             $table->string('email')->unique();
@@ -56,7 +56,7 @@ class CreateTables extends Migration
         });
 
         Schema::create('wink_pages', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->string('id')->primary();
             $table->string('slug')->unique();
             $table->string('title');
             $table->text('body');
